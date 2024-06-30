1191a76c2b7b7e29aaa8edee61df6617947f6516
Tay Nguyen <pttnguyen528@gmail.com>
2024-06-30 14:55:23 +0700

    delete component config

--- src/types/Project.itf.ts
+++ src/types/Project.itf.ts
@@ -1,69 +1,65 @@
 import { UserInfo } from "types";
-<<<<<<< HEAD
-import { IProjectCategory } from "./ProjectCategory.itf";
-=======
->>>>>>> 5b184e135bfa5a450bbd4656b581dcac4469ff58
 
 export interface ICreator {
-  name: string;
-  id: number;
+    name: string;
+    id: number;
 }
 
 export interface IProject {
-  id: number;
-  alias: string;
-  categoryId: number;
-  categoryName: string;
-  projectName: string;
-  description: string;
-  creator: ICreator;
-  members: Partial<UserInfo>[];
+    id: number;
+    alias: string;
+    categoryId: number;
+    categoryName: string;
+    projectName: string;
+    description: string;
+    creator: ICreator;
+    members: Partial<UserInfo>[];
 }
 
 export interface IProjectDetail {
-  project: IProject;
+    project: IProject;
 }
 
 export interface IProjectUpdate {
-  id: number;
-  projectName: string;
-  creator: number;
-  description: string;
-  categoryId: string;
+    id: number;
+    projectName: string;
+    creator: number;
+    description: string;
+    categoryId: string;
 }
 
 export interface IProjectEdit {
-  project: IProject;
+    project: IProject;
 }
 
 export interface IProjectActionBtns {
-  project: IProject;
+    project: IProject;
 }
 
 export interface IProjectMembers {
-  projectID: number;
-  projectName: string;
-  members: Partial<UserInfo>[];
+    projectID: number;
+    projectName: string;
+    members: Partial<UserInfo>[];
 }
 
 export interface IProjectMembersList {
-  members: Partial<IMember>[];
-  handleDeleteMember: (memberId: number) => void;
-  containerStyle?: string;
-  title?: string;
+    members: Partial<IMember>[];
+    handleDeleteMember: (memberId: number) => void;
+    containerStyle?: string;
+    title?: string;
 }
 
 export interface IMember {
-  userId: number;
-  name: string;
-  avatar: string;
+    userId: number;
+    name: string;
+    avatar: string;
 }
 
 export interface IProjectMembersAdd {
-  isMobile?: boolean;
-  title?: string;
-  projectName?: string;
-  containerClassName?: string;
-  userListClassName?: string;
-  handleAssignUser: (userId: number) => void;
+    isMobile?: boolean;
+    title?: string;
+    projectName?: string;
+    containerClassName?: string;
+    userListClassName?: string;
+    handleAssignUser: (userId: number) => void;
 }
